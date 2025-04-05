ce89ec3fd40c018bd8f234c045b1fed0f3ffd19a
Camilo Andrés Díaz Gómez <60698278+CamiloAndresDG@users.noreply.github.com>
2025-04-05 11:46:47 +0200

    Functional page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,15 +20,7 @@ export default function Footer() {
             </a>
           </div>
           <div className="text-gray-600 dark:text-gray-400">
-            {t('footer.credits')}{' '}
-            <a 
-              href="https://www.yan-holtz.com" 
-              className="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300"
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Yan Holtz
-            </a>
+            {t('footer.credits')} Yan Holtz
           </div>
         </div>
       </div>
